575b1a17e7af17412513586109e4bf863b902c49
Panferov.creator <panferov.creator@gmail.com>
2023-02-06 16:28:37 +0500

    Work with logic

--- HW_2.04(ColorizedApp)/ViewController.swift
+++ HW_2.04(ColorizedApp)/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.layer.cornerRadius = 15
+        
+        sliderAction()
     }
 
+    @IBAction func sliderAction() {
+        
+        redLabel.text = redSlider.value.formatted()
+        greenLabel.text = greenSlider.value.formatted()
+        blueLabel.text = blueSlider.value.formatted()
 
+        mainView.backgroundColor = UIColor(
+                displayP3Red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1
+        )
+    }
+    
 }
 
