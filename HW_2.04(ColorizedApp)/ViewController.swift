639125d680d6cc2d7afdba5e2bdf22e721aa6519
Panferov.creator <panferov.creator@gmail.com>
2023-02-06 12:42:26 +0500

    Initial Commit

---
+++ HW_2.04(ColorizedApp)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.04(ColorizedApp)
+//
+//  Created by Dmitriy Panferov on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
