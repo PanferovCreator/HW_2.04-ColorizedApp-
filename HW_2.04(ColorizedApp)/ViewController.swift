fd476ca0c1f597011800d00fff9b16a52f3290e7
Panferov.creator <panferov.creator@gmail.com>
2023-02-09 10:23:44 +0500

    Update stackView constraints

--- HW_2.04(ColorizedApp)/ViewController.swift
+++ HW_2.04(ColorizedApp)/ViewController.swift
@@ -22,26 +22,49 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         mainView.layer.cornerRadius = 15
-        
-        sliderAction()
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
         
     }
 
     // MARK: - IBActions
-    @IBAction func sliderAction() {
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: sender)
+        case greenSlider:
+            greenLabel.text = string(from: sender)
+        default:
+            blueLabel.text = string(from: sender)
+        }
+    }
         
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-
+    private func setColor() {
         mainView.backgroundColor = UIColor(
-                displayP3Red: CGFloat(redSlider.value),
-                green: CGFloat(greenSlider.value),
-                blue: CGFloat(blueSlider.value),
-                alpha: 1
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
         )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        return String(format: "%.2f", slider.value)
     }
     
 }
